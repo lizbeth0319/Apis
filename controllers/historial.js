8706a7108dbe906a016a623c23cdae6d3ff536bd
DuvanAndres <mantillatorresduvanandres@gmail.com>
2025-05-12 23:23:57 -0500

    Update historial.js

--- controllers/historial.js
+++ controllers/historial.js
@@ -1,4 +1,4 @@
-import historial from "../models/historial.js";
+/* import historial from "../models/historial.js";
 
 const gethistorial = async (req, res) => {
     try {
@@ -8,4 +8,17 @@ const gethistorial = async (req, res) => {
         res.status(400).json({ msg: "Error al buscar la historial" });
     }
 };
-export default httpArticulos
+export default httpArticulos */
+
+import Historial from "../models/Historial.js";
+
+const getHistorial = async (req, res) => {
+try {
+const historial = await Historial.find().sort({ createdAt: 1 });
+res.json(historial);
+} catch (error) {
+res.status(400).json({ msg: "Error al buscar el historial" });
+}
+};
+
+export default getHistorial;
